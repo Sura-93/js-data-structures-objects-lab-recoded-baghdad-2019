b146d2bdf19345b1d69bf7dbeddff3b79c758bdb
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-14 18:17:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ function destructivelyUpdateDriverWithKeyAndValue(driver,key, value){
   driver[key] = value;
 
   return driver;
- 
+
 }
 
 function deleteFromDriverByKey(driver,key){
-  
+
   newDriver=Object.assign({},driver);
   delete newDriver[key];
   return newDriver;
- 
+
 }
 
 function destructivelyDeleteFromDriverByKey(driver,key){
-  
+
   delete driver[key];
   return driver;
- 
+
 }
