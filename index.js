dc6c48040ebbffc4b7e6a3f63469d58cf43863a1
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-14 18:10:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+const driver = {key: ""};
+function updateDriverWithKeyAndValue(obj, key, value)
+
+{
+  driver[key]=value;
+  return driver;
+}
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
